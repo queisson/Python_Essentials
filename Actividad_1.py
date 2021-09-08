c1fbe3152656ff5f029c630f4ccadfaae3d8290e
queisson <90352170+queisson@users.noreply.github.com>
2021-09-08 16:58:06 -0500

    Actividad 1
    Desarrollo actividad 1

---
+++ Actividad_1.py
@@ -0,0 +1,25 @@
+print("Empezando a trabajar con Python")    #la funcion PRINT() imprime un tipo de datos str
+print("Realizado por:Paul Benalcazar")      #imprime un tipo de datos str
+print()                                     #la funcion PRINT() imprime un espacio en blanco
+print("Consultando los tipos de valores:")  #imprime un tipo de datos str
+print()                                     #se imprime un espacio en blanco
+print("El tipo de dato de 875 es:")         #la funcion PRINT() imprime un tipo de datos str
+print(type(857))                            #dentro de la funcion PRINT() se ejecuta la función type() nos dice el dato numérico, donde imprime tipo de dato INT
+print()                                     #se imprime un espacio en blanco
+print("El tipo de dato de 4.89 es:")        #la funcion PRINT() imprime un tipo de datos str
+print(type(4.89))                           #dentro de la funcion PRINT() ejecuta la función type() nos dice el dato numérico, donde imprime tipo de dato FLOAT
+print()                                     #se imprime un espacio en blanco
+print("El tipo de dato del texto: Now is better than never. es") # la funcion PRINT() imprime un tipo de datos str
+print(type("Now is better than never."))    #dentro de la funcion PRINT() ejecuta la función type() nos dice el dato numérico, donde imprime tipo de dato STR
+print()                                     #se imprime un espacio en blanco
+print("El tipo de dato de 1.32 es:")        #la funcion PRINT() imprime un tipo de datos str
+print(type(4.89))                           #dentro de la funcion PRINT() ejecuta la función type() nos dice el dato numérico, donde imprime tipo de dato FLOAT
+print()                                     #se imprime un espacio en blanco
+print("¿El valor 5 + 8i corresponde a un valor entero?")#la funcion PRINT() imprime un tipo de datos str
+print(isinstance(5 + 8j, int))              #dentro de la funcion PRINT() ejecuta la función ISINSTANCE(la parte imaginaria se cambia la i por la j) nos permite realizar la compararcion de valor, donde imprime FALSE
+print()                                     #se imprime un espacio en blanco
+print("¿El valor 8.2 corresponde a un valor entero?") #la funcion PRINT() imprime un tipo de datos str
+print(isinstance(8.2, int))                 #dentro de la funcion PRINT() ejecuta la función ISINSTANCE() nos permite realizar la compararcion de valor, donde imprime FALSE
+print()                                     #se imprime un espacio en blanco
+print("¿El texto: Readability counts. corresponde a un string?") #la funcion PRINT() imprime un tipo de datos str
+print(isinstance("Readability counts", str))#dentro de la funcion PRINT() ejecuta la función ISINSTANCE() nos permite realizar la compararcion de valor, donde imprime TRUE
